50125c5f60a1e3e6421746c3772a6ac2f32a2831
codesandbox-test <watoru374215@gmail.com>
2023-04-06 05:54:15 +0000

    10-56. 型定義 Omit

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,8 +1,6 @@
 import { TodoType } from "./types/todo";
 
-export const Todo = (
-  props: Pick<TodoType, "userId" | "title" | "completed">
-) => {
+export const Todo = (props: Omit<TodoType, "id">) => {
   const { title, userId, completed = false } = props;
   const completedMark = completed ? "[完]" : "[未]";
   return <p>{`${completedMark} ${title}(ユーザー:${userId})`}</p>;
