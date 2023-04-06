8d2ed239e6b4860221dcf3b94982ca080e8a58fd
watorutart <46208434+watorutart@users.noreply.github.com>
2023-04-06 14:33:32 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import "./styles.css";
+import axios from "axios";
+import { useState } from "react";
+import { Todo } from "./Todo.tsx";
+
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+export default function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+
+  const onClickFetchData = () => {
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
+  };
+
+  return (
+    <div className="App">
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} userId={todo.userId} />
+      ))}
+    </div>
+  );
+}
